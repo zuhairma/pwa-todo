6d3d1e2b260d43a0202efe04aa2656b366f682a8
Zuhair <zuhair_m_h@yahoo.com>
2024-09-04 09:08:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
         .then(function(registration) {
             console.log('Service Worker registered with scope:', registration.scope);
         }, function(error) {
