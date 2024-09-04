1fbb0112244cd3221467924ac4f9019c61b0fe0a
Zuhair <zuhair_m_h@yahoo.com>
2024-09-04 09:15:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `todo-list`;
+const CACHE_NAME = `/pwa-todo/todo-list`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
